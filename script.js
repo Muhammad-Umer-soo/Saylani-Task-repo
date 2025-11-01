41e31c960b9db5bc90ce4b4ef7eb7839754512ef
Muhammad-Umer-soo <muhammadumer11.2003@gmail.com>
2025-11-01 17:02:23 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var name = "Ahmed";
+var age = 25;
+alert(name)
+console.log(name);
+alert(age)
+console.log(age);
+var name = "Siraj";
+var age = 19;
+alert(name)
+console.log(name);
+alert(age)
+console.log(age);
+var My_name = "Umer";
+var myName = "Ahmed";
+var My$Name = "Ali";
+// var my name=''
+// var myname=''
